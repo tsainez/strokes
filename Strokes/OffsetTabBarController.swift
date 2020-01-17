752fa44afbf59affe6da6b3fc4325bef69875214
Anthony Sainez <tsainez@gmail.com>
2020-01-17 00:48:02 -0800

    Create OffsetTabBarController.swift

---
+++ Strokes/OffsetTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  OffsetTabBarController.swift
+//  Strokes
+//
+//  Created by Anthony Sainez on 16 January 2020.
+//  Copyright © 2020 Anthony Sainez. All rights reserved.
+//
+
+import UIKit
+
+class OffsetTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1 // default to Canvas View
+    }
+}
